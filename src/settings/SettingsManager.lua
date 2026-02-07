5ac35072c080963e9b52b46cc073a521468a055b
TisonK <tisonklomp1@gmail.com>
2026-02-07 06:06:22 +0100

    Update SettingsManager.lua

--- src/settings/SettingsManager.lua
+++ src/settings/SettingsManager.lua
@@ -1,5 +1,5 @@
 -- =========================================================
--- FS25 Realistic Soil & Fertilizer (version 1.0.1.1)
+-- FS25 Realistic Soil & Fertilizer (version 1.0.1.2)
 -- =========================================================
 -- Realistic soil fertility and fertilizer management
 -- =========================================================
@@ -25,7 +25,11 @@ SettingsManager.defaultConfig = {
     fertilitySystem = true,
     nutrientCycles = true,
     fertilizerCosts = true,
-    showNotifications = true
+    showNotifications = true,
+    -- NEW SETTINGS DEFAULT CONFIG
+    seasonalEffects = true,
+    rainEffects = true,
+    plowingBonus = true
 }
 
 function SettingsManager.new()
@@ -52,6 +56,10 @@ function SettingsManager:loadSettings(settingsObject)
             settingsObject.nutrientCycles = xml:getBool(self.XMLTAG..".nutrientCycles", self.defaultConfig.nutrientCycles)
             settingsObject.fertilizerCosts = xml:getBool(self.XMLTAG..".fertilizerCosts", self.defaultConfig.fertilizerCosts)
             settingsObject.showNotifications = xml:getBool(self.XMLTAG..".showNotifications", self.defaultConfig.showNotifications)
+            -- NEW SETTINGS LOAD
+            settingsObject.seasonalEffects = xml:getBool(self.XMLTAG..".seasonalEffects", self.defaultConfig.seasonalEffects)
+            settingsObject.rainEffects = xml:getBool(self.XMLTAG..".rainEffects", self.defaultConfig.rainEffects)
+            settingsObject.plowingBonus = xml:getBool(self.XMLTAG..".plowingBonus", self.defaultConfig.plowingBonus)
             
             xml:delete()
             return
@@ -64,6 +72,10 @@ function SettingsManager:loadSettings(settingsObject)
     settingsObject.nutrientCycles = self.defaultConfig.nutrientCycles
     settingsObject.fertilizerCosts = self.defaultConfig.fertilizerCosts
     settingsObject.showNotifications = self.defaultConfig.showNotifications
+    -- NEW SETTINGS DEFAULT LOAD
+    settingsObject.seasonalEffects = self.defaultConfig.seasonalEffects
+    settingsObject.rainEffects = self.defaultConfig.rainEffects
+    settingsObject.plowingBonus = self.defaultConfig.plowingBonus
 end
 
 function SettingsManager:saveSettings(settingsObject)
@@ -80,6 +92,10 @@ function SettingsManager:saveSettings(settingsObject)
         xml:setBool(self.XMLTAG..".nutrientCycles", settingsObject.nutrientCycles)
         xml:setBool(self.XMLTAG..".fertilizerCosts", settingsObject.fertilizerCosts)
         xml:setBool(self.XMLTAG..".showNotifications", settingsObject.showNotifications)
+        -- NEW SETTINGS SAVE
+        xml:setBool(self.XMLTAG..".seasonalEffects", settingsObject.seasonalEffects)
+        xml:setBool(self.XMLTAG..".rainEffects", settingsObject.rainEffects)
+        xml:setBool(self.XMLTAG..".plowingBonus", settingsObject.plowingBonus)
         
         xml:save()
         xml:delete()
